5b5e57980053a5b597b934f3cee9d0cf7661382d
Emil-Maris <91554244+Emil-Maris@users.noreply.github.com>
2021-10-25 19:49:11 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-'''
+"""
 Găsește ultimul număr prim mai mic decât un număr dat.
-'''
+"""
+
 
 def v_prim(x):
     p = 0
@@ -22,63 +23,61 @@ def get_largest_prime_below(m):
 n = int(input("n="))
 get_largest_prime_below(n)
 
-
 '''
 Transformă un număr dat din baza 10 în baza 2. Numărul se dă în baza 10.
 '''
 
+
 def get_base_2(x):
-    a = []
-    b = 0
-    while x != 0:
-        p = x % 2
-        a.append(p)
-        x = int(x / 2)
     p = 1
-    for i in range(len(a) - 1, -1, -1):
-        b = b + a[i] * p
+    x2 = 0
+    while x:
+        x2 = x2 + int((x % 2)) * p
         p = p * 10
-    return b
+        x = x / 2
+    return x2
 
 
 n = int(input())
 print(get_base_2(n))
 
-
 '''
 Determină dacă un număr este antipalindrom.
 '''
 
-def is_antipalindrome(n):
 
+def is_antipalindrome(n):
     n = str(n)
     l = len(n) - 1
     i = 0
     ok = 1
-    while i < l :
-        if n[i]==n[l]:
-            ok=0
-        i=i+1
-        l=l-1
-    if ok==0:
+    while i < l:
+        if n[i] == n[l]:
+            ok = 0
+        i = i + 1
+        l = l - 1
+    if ok == 0:
         return False
-    else: return True
+    else:
+        return True
 
-option = '''
-Daca vreti sa gasiti ultimul nr prim mai mic decat o valoare data, tastati "1".
-Daca vreti sa transformati un numar din baza 10 in baza 2, tastati "2".
-Daca vreti sa verificati daca un numar este antipalindrom, tastati "3".
-Daca vreti sa inchideti programul, tastati "4".
-'''
+
+def meniu():
+    print("Daca vreti sa gasiti ultimul nr prim mai mic decat o valoare data, tastati 1.")
+    print("Daca vreti sa transformati un numar din baza 10 in baza 2, tastati 2.")
+    print("Daca vreti sa verificati daca un numar este antipalindrom, tastati 3.")
+    print("Daca vreti sa inchideti programul, tastati 4.")
+
 
 assert get_largest_prime_below(25) == 23
-    assert get_largest_prime_below(2) == None
-    assert get_largest_prime_below(30) == 29
-    
+assert get_largest_prime_below(2) is None
+assert get_largest_prime_below(15) == 13
+
 assert get_base_2('10') == '1010'
-    assert get_base_2('54') == '110110'
-    assert get_base_2('16') == '10000'
-    assert get_base_2('64') == '1000000'
+assert get_base_2('54') == '110110'
+assert get_base_2('16') == '10000'
+assert get_base_2('64') == '1000000'
+
 
 def test_is_antipalindrome():
     assert is_antipalindrome(620) is True
@@ -86,24 +85,28 @@ def test_is_antipalindrome():
     assert is_antipalindrome(232) is False
     assert is_antipalindrome(162) is True
 
+
 def main():
     while True:
-        optiune = input(option)
+        meniu()
+        optiune = input("Alege-ti optiunea: ")
         if optiune == '1':
-            nr = int(input("SCrieti numarul: "))
-            print("Cel mai mare numar prim, mai mic decat {nr}, este: + str(get_largest_prime_below(nr)))
+            nr = int(input("Scrieti numarul: "))
+            print("Cel mai mare numar prim, mai mic decat " + str(nr) + ", este: " + str(get_largest_prime_below(nr)))
         elif optiune == '2':
-            nr= int(input("Scrieti numarul pe care doriti sa-l convertiti din baza 10 in baza 2: "))
-            print("Numarul {nr} convertit din baza 10 in baza 2 este: " + get_base_2(numar))
+            nr = int(input("Scrieti numarul pe care doriti sa-l convertiti din baza 10 in baza 2: "))
+            print("Numarul " + str(nr) + "convertit din baza 10 in baza 2 este: " + get_base_2(nr))
         elif optiune == '3':
-            nr = int(input("Scrieti numarul care se va determina daca este antipalindrom: ")
-            if is_antipalindrome(nr) == False:
+            nr = int(input("Scrieti numarul care se va determina daca este antipalindrom: "))
+            if not is_antipalindrome(nr):
                 print(f"Numarul {nr} nu este antipalindrom.")
-            else: print(f"Numarul {nr} este antipalindrom.")
-         elif optiune == '4':
+            else:
+                print(f"Numarul {nr} este antipalindrom.")
+        elif optiune == '4':
             print("Programul s-a oprit.")
             break
-        else: print("Comanda gresita! Incercati din nou.")
+        else:
+            print("Comanda gresita! Incercati din nou.")
 
-if __name__ == '__main__':
-  main()          
+
+main()
