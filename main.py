c6d749c800a08b22e64fd4efc5014a4768fb1c59
Emil-Maris <emil.maris@stud.ubbcluj.ro>
2021-11-01 18:43:17 +0200

    laboratorul 2

--- main.py
+++ main.py
@@ -1,112 +1,108 @@
-"""
-Găsește ultimul număr prim mai mic decât un număr dat.
-"""
+def invers(n):
+    '''
+    Returnează inversul unui nr.
+    :param n: numărul citit
+    :return: inversul nr. citit
+    '''
+    inversul = 0
+    while n > 0:
+        inversul = inversul*10 + n % 10
+        n = n//10
+    return inversul
 
-
-def v_prim(x):
-    p = 0
-    for i in range(2, int(x / 2) + 1):
-        if x % i == 0:
-            p = 1
-            break
-    return p
-
-
-def get_largest_prime_below(m):
-    while m != 0:
-        if v_prim(m) == 0:
-            print(m)
-            break
-        m = m - 1
-
-
-n = int(input("n="))
-get_largest_prime_below(n)
-
-'''
-Transformă un număr dat din baza 10 în baza 2. Numărul se dă în baza 10.
-'''
-
-
-def get_base_2(x):
-    p = 1
-    x2 = 0
-    while x:
-        x2 = x2 + int((x % 2)) * p
-        p = p * 10
-        x = x / 2
-    return x2
-
-
-n = int(input())
-print(get_base_2(n))
-
-'''
-Determină dacă un număr este antipalindrom.
-'''
-
-
-def is_antipalindrome(n):
-    n = str(n)
-    l = len(n) - 1
-    i = 0
-    ok = 1
-    while i < l:
-        if n[i] == n[l]:
-            ok = 0
-        i = i + 1
-        l = l - 1
-    if ok == 0:
-        return False
+def is_palindrome(n):
+    '''
+    Verifică dacă un nr citit de la tastatură este palindrom.
+    :param n: numărul citit
+    :return: true sau false
+    '''
+    if n == invers(n):
+        return True
     else:
+        return False
+
+def isPrime(n):
+    '''
+    Verifică dacă un nr. este prim.
+    :param n: nr citit
+    :return: true sau false
+    '''
+    if n < 2:
+        return False
+    if n == 2:
+        return True
+    if n >= 2:
+        for i in range(2, n):
+            if n % i == 0:
+                return False
         return True
 
 
-def meniu():
-    print("Daca vreti sa gasiti ultimul nr prim mai mic decat o valoare data, tastati 1.")
-    print("Daca vreti sa transformati un numar din baza 10 in baza 2, tastati 2.")
-    print("Daca vreti sa verificati daca un numar este antipalindrom, tastati 3.")
-    print("Daca vreti sa inchideti programul, tastati 4.")
+def is_superprime(n):
+    '''
+    Verifică dacă un nr este superprim.
+    :param n: nr citit
+    :return: true sau false
+    '''
+    if n < 2:
+        return False
+    while n > 0:
+        if n < 2:
+            return False
+        if isPrime(n) == False:
+            return False
+        n = n // 10
+    return True
 
+def get_largest_prime_below(n):
+    '''
+    Găsește cel mai mare nr. prim mai mic decât cel dat
+    :param n: nr dat
+    :return: ultimul numar prim mai mic decat n
+    '''
+    for i in range(n-1, 1, -1):
+         if isPrime(i):
+            return i
+    return 2
 
-assert get_largest_prime_below(25) == 23
-assert get_largest_prime_below(2) is None
-assert get_largest_prime_below(15) == 13
+def test_is_palindrom():
+    assert is_palindrome(626) == True
+    assert is_palindrome(4424) == False
+    assert is_palindrome(5) == True
 
-assert get_base_2('10') == '1010'
-assert get_base_2('54') == '110110'
-assert get_base_2('16') == '10000'
-assert get_base_2('64') == '1000000'
+def test_is_superprime():
+    assert is_superprime(233) is True
+    assert is_superprime(237) is False
+    assert is_superprime(72) is False
 
-
-def test_is_antipalindrome():
-    assert is_antipalindrome(620) is True
-    assert is_antipalindrome(2312) is False
-    assert is_antipalindrome(232) is False
-    assert is_antipalindrome(162) is True
-
+def test_get_largest_prime_below():
+    assert get_largest_prime_below(6) == 5
+    assert get_largest_prime_below(10) == 7
+    assert get_largest_prime_below(200) == 199
 
 def main():
     while True:
-        meniu()
-        optiune = input("Alege-ti optiunea: ")
-        if optiune == '1':
-            nr = int(input("Scrieti numarul: "))
-            print("Cel mai mare numar prim, mai mic decat " + str(nr) + ", este: " + str(get_largest_prime_below(nr)))
-        elif optiune == '2':
-            nr = int(input("Scrieti numarul pe care doriti sa-l convertiti din baza 10 in baza 2: "))
-            print("Numarul " + str(nr) + "convertit din baza 10 in baza 2 este: " + get_base_2(nr))
-        elif optiune == '3':
-            nr = int(input("Scrieti numarul care se va determina daca este antipalindrom: "))
-            if not is_antipalindrome(nr):
-                print(f"Numarul {nr} nu este antipalindrom.")
+        print("1. Determină dacă un număr dat este palindrom. ")
+        print("2. Determină dacă un număr este superprim. ")
+        print("3. Găsește ultimul număr prim mai mic decât un număr dat. ")
+        print("x. Inchide programul.")
+        optiune = input("Opțiunea: ")
+        if optiune == "1":
+            n= int(input("n = "))
+            if is_palindrome(n):
+                print("Numărul dat este palindrom.")
             else:
-                print(f"Numarul {nr} este antipalindrom.")
-        elif optiune == '4':
-            print("Programul s-a oprit.")
-            break
+                print("Numărul dat NU este palindrom.")
+        elif optiune == "2":
+            n = int(input("n = "))
+            print(is_superprime(n))
+        elif optiune == "3":
+            n = int(input("n = "))
+            prime = get_largest_prime_below(n)
+            print("Ultimul nr. prim mai mic decat {} este {}".format(n, prime))
+        elif optiune == "x":
+            return
         else:
-            print("Comanda gresita! Incercati din nou.")
-
+            print ("Opțiune invalidă! Reîncercați: ")
 
 main()
